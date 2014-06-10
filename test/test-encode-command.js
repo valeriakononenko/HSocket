2094ae0b2a6d063d297cac103dee0566647ee16a
Valeria Lepina <divergence082@gmail.com>
2014-06-10 20:14:55 +0400

    parsing

--- test/test-encode-command.js
+++ test/test-encode-command.js
@@ -4,13 +4,14 @@ var hsocket = require('../bin/index.js');
 
 var client = new hsocket.Client(9998, 9999);
 //var id = Math.floor(Math.random()*100 + 1);
-var id = 1;
-var index = new hsocket.Index(id, 'test', null,
-    ['time']);
-console.log('id:', id);
+//var id = 2;
+var index = new hsocket.Index('test', 'hsocket', ['time']);
+console.log('id:', index.getId());
 
-client.openIndex(index, function() {
-  client.insert([Date.now()], function() {
+client.openIndex(index, function(result) {
+  console.log('RESULT:', result);
+  client.insert([13], function() {
     console.log('END');
+
   }, console.error);
 }, console.error);
