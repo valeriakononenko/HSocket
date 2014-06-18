f437334a175fca743c5cac2d3655b8b4c887a4db
Valeria Lepina <divergence082@gmail.com>
2014-06-18 19:41:17 +0400

    tested commands

--- test/test-encode-command.js
+++ test/test-encode-command.js
@@ -4,16 +4,25 @@ var hsocket = require('../bin/index.js');
 
 var client = new hsocket.Client(9998, 9999);
 var index = new hsocket.Index('test', 'hs', ['id', 'c1', 'c2']);
-index.setId(19);
 console.log('id:', index.getId());
 
 client.openIndex(index, function() {
   console.log('INDEX OK');
-  client.insert([23, 1, 1], function() {
-    console.log('INSERT 1 OK');
+
+//  var limit = hsocket.constraints.LIMIT(10, 0);
+//  client.find('=', [1], function(result) {
+//    console.log('FIND OK', result);
+//  }, function(error, code) {
+//    console.log('FIND ERROR:', code, error);
+//  }, limit);
+
+  client.delete('=', [2], function(result) {
+    console.log('DELETE OK', result);
   }, function(error, code) {
-    console.log('INSERT 1 ERROR:', code, error);
-  });
+    console.log('DELETE ERROR:', code, error);
+  })
+
+
 }, function(error, code) {
   console.log('INDEX ERROR:', code, error);
 });
